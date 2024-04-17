a0a883366fba7e2bda3c5d1410cc4af260e2ae14
Farman Ali <farmanali6349@gmail.com>
2024-04-17 15:09:52 +0500

    Updated loading at login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,7 +3,7 @@ import authService from '../../appwrite/auth'
 import { login as authLogin } from '../../store/features/authSlice'
 import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from "react-hook-form"
-import { Input, Button, Logo } from "../index"
+import { Input, Button, Loading } from "../index"
 import "./Login.css"
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -11,12 +11,14 @@ import { Link, useNavigate } from 'react-router-dom';
 function Login() {
 
     const [errors, setErrors] = useState("");
+    const [loading, setLoading] = useState(false);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { register, handleSubmit } = useForm();
 
 
     const login = async (loginData) => {
+        setLoading(true);
         authService.login({ ...loginData })
             .then((session) => {
 
@@ -25,50 +27,64 @@ function Login() {
                 authService.getCurrentUser()
                     .then((userData) => dispatch(authLogin({ userData })))
                     .catch((err) => console.log("Error Occured while fetching user data after login."))
+                    .finally(() => setLoading(false))
 
             }).catch((err) => console.log("Error Occured During Login :: Error -> ", err))
+            .finally(async () => {
+                const loadingTimeOut = await setTimeout(()=> {
+                    setLoading(false);
+                }, 1000)
+
+                clearTimeout(loadingTimeOut);
+
+
+            })
 
     }
 
 
     return (
-        <form className='login-form stroke' onSubmit={handleSubmit(login)}>
-            <Input
-                type="email"
-                label="Enter your email:"
-                placeholder="example@example.com"
-                className="input stroke"
-                {...register("email", {
-                    required: true,
-                    validate: {
-                        matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-                            "Email address must be a valid address"
-                    }
-                })}
+        <>
+            {!loading ? (
+                <form className='login-form stroke' onSubmit={handleSubmit(login)}>
+                    <Input
+                        type="email"
+                        label="Enter your email:"
+                        placeholder="example@example.com"
+                        className="input stroke"
+                        {...register("email", {
+                            required: true,
+                            validate: {
+                                matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                                    "Email address must be a valid address"
+                            }
+                        })}
 
-            />
-            <Input
-                type="password"
-                label="Enter your password:"
-                placeholder="*****"
-                className="input stroke"
-                {...register("password", {
-                    required: true
-                })}
+                    />
+                    <Input
+                        type="password"
+                        label="Enter your password:"
+                        placeholder="*****"
+                        className="input stroke"
+                        {...register("password", {
+                            required: true
+                        })}
 
-            />
-            <Button className="button w-100" type="submit">
-                Login
-            </Button>
-            <p>Do not have an account? {(
-                <>
-                    <Link to="/Register">
-                        Register
-                    </Link>
+                    />
+                    <Button className="button w-100" type="submit">
+                        Login
+                    </Button>
+                    <p>Do not have an account? {(
+                        <>
+                            <Link to="/Register">
+                                Register
+                            </Link>
 
-                </>
-            )}</p>
-        </form>
+                        </>
+                    )}</p>
+                </form>
+            ) : <Loading />}
+        </>
     )
 }
 
