67e5c7862f65ae9c77fb5ddeda93c9798a59ea24
Farman Ali <farmanali6349@gmail.com>
2024-04-17 23:11:35 +0500

    Updated React  Loading Bar At Login Component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import authService from '../../appwrite/auth'
 import { login as authLogin } from '../../store/features/authSlice'
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,9 +6,15 @@ import { useForm } from "react-hook-form"
 import { Input, Button, Loading } from "../index"
 import "./Login.css"
 import { Link, useNavigate } from 'react-router-dom';
+import LoadingBar from "react-top-loading-bar"
 
 
 function Login() {
+
+    // Loading bar
+    const loadingBarColor = "#a75bff"
+    const loadingBarRef = useRef(null);
+    const [loadingProgress, setLoadingProgress] = useState(0);
 
     const [errors, setErrors] = useState("");
     const [loading, setLoading] = useState(false);
@@ -18,7 +24,9 @@ function Login() {
 
 
     const login = async (loginData) => {
+        loadingBarRef.current.continuousStart();
         setLoading(true);
+
         authService.login({ ...loginData })
             .then((session) => {
 
@@ -30,62 +38,55 @@ function Login() {
                     .finally(() => setLoading(false))
 
             }).catch((err) => console.log("Error Occured During Login :: Error -> ", err))
-            .finally(async () => {
-                const loadingTimeOut = await setTimeout(()=> {
-                    setLoading(false);
-                }, 1000)
+            .finally(()=> loadingBarRef.current.complete()).finally(() => loadingBarRef.current.complete())
 
-                clearTimeout(loadingTimeOut);
+}
 
 
-            })
+return (
+    <>
+        <LoadingBar color={loadingBarColor} ref={loadingBarRef} progress={loadingProgress} />
+        {!loading ? (
+            <form className='login-form stroke' onSubmit={handleSubmit(login)}>
+                <Input
+                    type="email"
+                    label="Enter your email:"
+                    placeholder="example@example.com"
+                    className="input stroke"
+                    {...register("email", {
+                        required: true,
+                        validate: {
+                            matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                                "Email address must be a valid address"
+                        }
+                    })}
 
-    }
+                />
+                <Input
+                    type="password"
+                    label="Enter your password:"
+                    placeholder="*****"
+                    className="input stroke"
+                    {...register("password", {
+                        required: true
+                    })}
 
+                />
+                <Button className="button w-100" type="submit">
+                    Login
+                </Button>
+                <p>Do not have an account? {(
+                    <>
+                        <Link to="/Register">
+                            Register
+                        </Link>
 
-    return (
-        <>
-            {!loading ? (
-                <form className='login-form stroke' onSubmit={handleSubmit(login)}>
-                    <Input
-                        type="email"
-                        label="Enter your email:"
-                        placeholder="example@example.com"
-                        className="input stroke"
-                        {...register("email", {
-                            required: true,
-                            validate: {
-                                matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
-                                    "Email address must be a valid address"
-                            }
-                        })}
-
-                    />
-                    <Input
-                        type="password"
-                        label="Enter your password:"
-                        placeholder="*****"
-                        className="input stroke"
-                        {...register("password", {
-                            required: true
-                        })}
-
-                    />
-                    <Button className="button w-100" type="submit">
-                        Login
-                    </Button>
-                    <p>Do not have an account? {(
-                        <>
-                            <Link to="/Register">
-                                Register
-                            </Link>
-
-                        </>
-                    )}</p>
-                </form>
-            ) : <Loading />}
-        </>
-    )
+                    </>
+                )}</p>
+            </form>
+        ) : <Loading />}
+    </>
+)
 }
 
 export default Login
